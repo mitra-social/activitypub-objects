659995cda2f9d0bef9864c5c283223d7bad031e7
Franco Gervasi <fgervasi12@gmail.ch>
2020-06-03 11:03:08 +0200

    Config lint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,21 @@
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
-  extends: ["eslint:recommended"],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
   overrides: [
     {
       files: [
         "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
       ],
       env: {
-        jest: true,
-      },
-    },
-  ],
+        jest: true
+      }
+    }
+  ]
 };
